78dafef198054795ce0dc4644674f0690b1cf561
Yaroslav Pleshakov <yvnger@gmail.com>
2023-12-02 22:56:50 +0400

    Added BaseNavigation component with routes

---
+++ src/components/BaseNavigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="header__nav nav-header">
+    <ul class="nav-header__items">
+      <li class="nav-header__item">
+        <router-link class="nav-header__link" to="/"> Главная </router-link>
+      </li>
+      <li class="nav-header__item nav-header__item--dropdown">
+        <a class="nav-header__link" href="#" @click.prevent>
+          Компетенции
+          <div class="nav-header__dropdown dropdown">
+            <ul class="dropdown__items">
+              <li class="dropdown__item">
+                <router-link to="/technologies" class="dropdown__link">
+                  Производственные технологии
+                </router-link>
+              </li>
+              <li class="dropdown__item">
+                <router-link to="/projects" class="dropdown__link">
+                  Реализованные проекты
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </a>
+      </li>
+      <li class="nav-header__item">
+        <router-link class="nav-header__link" to="/about">
+          О компании
+        </router-link>
+      </li>
+      <li class="nav-header__item">
+        <router-link class="nav-header__link" to="/contacts">
+          Контакты</router-link
+        >
+      </li>
+    </ul>
+
+    <button class="nav-header__hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  </nav>
+</template>
