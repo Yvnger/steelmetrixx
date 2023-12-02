6f9e70e6b3912137da90cb3bcaa8af6580793366
Yaroslav Pleshakov <yvnger@gmail.com>
2023-12-02 22:56:32 +0400

    Added BaseHeader component

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <router-link to="." class="header__logo-link">
+        <img
+          class="header__logo"
+          src="@/assets/images/logo.svg"
+          alt="Логотип"
+        />
+      </router-link>
+
+      <BaseNavigation />
+
+      <a href="tel:88003027479" class="header__phone">8 800 302 74 79</a>
+    </div>
+  </header>
+</template>
+
+<script>
+import BaseNavigation from '@/components/BaseNavigation.vue'
+
+export default {
+  components: { BaseNavigation },
+}
+</script>
