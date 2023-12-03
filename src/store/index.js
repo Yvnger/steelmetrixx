39ababbed089fff0361afd8513cf01c08587d02e
Yaroslav Pleshakov <yvnger@gmail.com>
2023-12-04 00:22:33 +0400

    Added navItems and getter for this

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,40 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
-  getters: {},
+  state: {
+    navItems: [
+      {
+        anchor: 'Главная',
+        path: 'home',
+      },
+      {
+        anchor: 'Компетенции',
+        dropdown: [
+          {
+            anchor: 'Производственные технологии',
+            path: 'tehnologies',
+          },
+          {
+            anchor: 'Реализованные проекты',
+            path: 'projects',
+          },
+        ],
+      },
+      {
+        anchor: 'О компании',
+        path: 'about',
+      },
+      {
+        anchor: 'Контакты',
+        path: 'contacts',
+      },
+    ],
+  },
+  getters: {
+    getNavItems(state) {
+      return state.navItems
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
