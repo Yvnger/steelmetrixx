dcf9aa39d47448c0b7de3e4df73325c22c4b321c
Yaroslav Pleshakov <yvnger@gmail.com>
2023-12-04 00:20:21 +0400

    Separated HeaderNav to another components

---
+++ src/components/HeaderNavSingleLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <RouterLink
+      class="nav-header__link"
+      :to="{ name: route.path }"
+      v-if="!route.dropdown"
+  >
+    {{ route.anchor }}
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  props: ['route']
+}
+</script>
