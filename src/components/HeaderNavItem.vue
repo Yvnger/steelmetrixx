f81ef5b88d4739e2464bba5f0c0849945411e452
Yaroslav Pleshakov <yvnger@gmail.com>
2023-12-04 00:30:54 +0400

    Added list item element with 2 variations links - single and dropdown

--- src/components/HeaderNavItem.vue
+++ src/components/HeaderNavItem.vue
@@ -1,13 +1,20 @@
 <template>
-$END$
+  <li
+    class="nav-header__item"
+    :class="{ 'nav-header__item--dropdown': route.dropdown }"
+  >
+    <HeaderNavSingleLink :route="route" v-if="!route.dropdown" />
+
+    <HeaderNavDropdownLink :route="route" v-else />
+  </li>
 </template>
 
 <script>
+import HeaderNavSingleLink from '@/components/HeaderNavSingleLink.vue'
+import HeaderNavDropdownLink from '@/components/HeaderNavDropdownLink.vue'
+
 export default {
-name: "HeaderNavItem"
+  props: ['route'],
+  components: { HeaderNavSingleLink, HeaderNavDropdownLink },
 }
 </script>
-
-<style scoped>
-
-</style>
